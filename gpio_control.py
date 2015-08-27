412555b751af68aad7f5354e6cb4c6cd22b89ff3
nico <nico.marcq@gmail.com>
2015-08-27 22:46:50 +0200

    api now use glpio lib

--- gpio_control.py
+++ gpio_control.py
@@ -1,5 +1,8 @@
 from threading import Thread
 import time
+import yaml
+import os
+import subprocess
 
 
 class GpioControl(Thread):
@@ -10,9 +13,30 @@ class GpioControl(Thread):
         self.timeout = timeout
         self.slot = slot
 
+        __location__ = os.path.realpath(
+        os.path.join(os.getcwd(), os.path.dirname(__file__)))
+        with open(os.path.join(__location__, "settings.yml")) as ymlfile:
+            self.cfg = yaml.load(ymlfile)
+            print self.cfg
+
     def run(self):
-        print 'Switch GPIO slot '+str(self.slot)+' on for '+str(self.timeout)+' seconde'
-        time.sleep(int(self.timeout))
-        # TODO: switch gpio port to 1
-        print 'Switch GPIO slot '+str(self.slot)+' back off'
-        # TODO: switch gpio port to 0
+        # convert slot number into GPIO port number
+        gpio_id = self.cfg['gpio_mapping'][int(self.slot)]
+
+        if self.cfg['active_gpio']:
+            print 'Switch GPIO slot '+str(gpio_id)+' on for '+str(self.timeout)+' seconde'
+            self._execute_script(gpio_id, "HIGH")
+            # wait the timout before shutting down the pump
+            time.sleep(int(self.timeout))
+            print 'Switch GPIO slot '+str(gpio_id)+' back off'
+            self._execute_script(gpio_id, "LOW")
+        else:
+            print 'Fake switch GPIO slot '+str(gpio_id)+' on for '+str(self.timeout)+' seconde'
+            time.sleep(int(self.timeout))
+            print 'Fake switch GPIO slot '+str(gpio_id)+' back off'
+
+    def _execute_script(self, port, status):
+        cmd = "sudo python switch_gpio_port_status.py --port "+str(port)+" --status "+status
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+        (output, err) = p.communicate()
+        # print "Output:", output
