b0a856611fbb9f2d43206c34bd47e9ade7d11a6c
nico <nico.marcq@gmail.com>
2015-08-29 14:12:13 +0200

    test each case: create cocktail , active/ disable one/all pump, on/off reverse pump

--- test.py
+++ test.py
@@ -16,6 +16,9 @@ filelock.create_lock_file()
 # -------------------------
 # Test API
 # -------------------------
+# local test
+# url = 'http://localhost:5000'
+# RPI2 test
 url = 'http://192.168.0.22:5000'
 headers = {'content-type': 'application/json'}
 # get /
@@ -23,24 +26,45 @@ headers = {'content-type': 'application/json'}
 
 # Test create cocktail
 payload = {'data': [
-    {'slot_id': '5', 'volume': '11'},
-    {'slot_id': '1', 'volume': '5'}]}
+    {'slot_id': '1', 'volume': '5'},
+    {'slot_id': '5', 'volume': '11'}]}
 r = post(url+'/make_cocktail', data=json.dumps(payload), headers=headers)
 print r.text
 """
+
 # Test start specific pump
-payload = {'slot_id': '13', 'action': 'start'}
+payload = {'slot_id': '4', 'action': 'start'}
 r = post(url+'/active_pump', data=json.dumps(payload), headers=headers)
 print r.text
 
+
 # Test stop specific pump
-payload = {'slot_id': '13', 'action': 'stop'}
+payload = {'slot_id': '4', 'action': 'stop'}
 r = post(url+'/active_pump', data=json.dumps(payload), headers=headers)
 print r.text
+
 
 # Test start all pump
 payload = {'action': 'start'}
 r = post(url+'/active_pump', data=json.dumps(payload), headers=headers)
 print r.text
-"""
 
+
+# Test stop all pump
+payload = {'action': 'stop'}
+r = post(url+'/active_pump', data=json.dumps(payload), headers=headers)
+print r.text
+
+
+# Test reverse pump On
+payload = {'action': 'on'}
+r = post(url+'/reverse_pump', data=json.dumps(payload), headers=headers)
+print r.text
+
+
+# Test reverse pump Off
+payload = {'action': 'off'}
+r = post(url+'/reverse_pump', data=json.dumps(payload), headers=headers)
+print r.text
+
+"""
