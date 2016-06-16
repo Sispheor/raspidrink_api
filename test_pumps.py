dcc6f2795c9636fd79a5c529a5154b3a7df173a8
nico <nico.marcq@gmail.com>
2016-06-16 19:02:59 +0200

    fix test pump

--- test_pumps.py
+++ test_pumps.py
@@ -11,13 +11,13 @@ cfg = get_settings()
 # First set all to 0
 for slot in cfg['gpio_mapping']:
     # convert slot number into GPIO port number
-    gpio_id = cfg['gpio_mapping'][int(slot)]
+    gpio_id = cfg['gpio_mapping'][int(slot)]['pin_number']
     execute_script(gpio_id, "LOW")
 
 # Set all to 1 durring 1 second
 for slot in cfg['gpio_mapping']:
     # convert slot number into GPIO port number
-    gpio_id = cfg['gpio_mapping'][int(slot)]
+    gpio_id = cfg['gpio_mapping'][int(slot)]['pin_number']
     print "GPIO "+str(gpio_id)+" HIGH"
     execute_script(gpio_id, "HIGH")
     time.sleep(1)
